139f055defba25ed4304b69ad9d602c0059c69e3
Edgar Adrian <edgar.cordero2890@alumnos.udg.mx>
2020-11-01 18:24:59 -0600

    Incluye las inserciones y eliminaciones

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "array.h"
+
+using namespace std;
+
+int main()
+{
+    Array<string> a;
+
+    a.insertAtEnd("EDGAR");
+    a.insertAtEnd("CORDERO");
+
+    a.insertAtStart("MUNDO");
+    a.insertAtStart("HOLA");
+
+    for (size_t i = 0; i < a.sizeofArray(); i++){
+        cout << a[i] << endl;
+    }
+    cout << endl;
+
+    a.insertar("HELLO", 2);
+
+    for (size_t i = 0; i < a.sizeofArray(); i++){
+        cout << a[i] << endl;
+    }
+    cout << endl;
+
+    a.eliminar(1);
+
+    for (size_t i = 0; i < a.sizeofArray(); i++){
+        cout << a[i] << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
